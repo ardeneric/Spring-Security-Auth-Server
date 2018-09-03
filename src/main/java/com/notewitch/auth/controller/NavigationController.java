5d411cce973c8e491c23814e4020d65ebedf1c3d
Eric Arden <earden@IM-21-B1-005.local>
2018-09-03 17:43:11 +0000

    testing

--- src/main/java/com/notewitch/auth/controller/NavigationController.java
+++ src/main/java/com/notewitch/auth/controller/NavigationController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.notewitch.auth.entity.Role;
 import com.notewitch.auth.entity.User;
 import com.notewitch.auth.repository.UserRepository;
 
@@ -36,6 +37,9 @@ public class NavigationController {
 			model.addAttribute("userPresent", true);
 		}else {
 			log.info("Saving new user ::" + user.getUsername() );
+			Role r = new Role();
+			r.setId(1);
+			user.setRoleId(r);
 			userRepository.save(user);
 		}
 		return "redirect:/login";
